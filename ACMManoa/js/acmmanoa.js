82ef23a408c93c8fb1ebc31e1db473f4aa9beb41
torleif-nielsen <torleifnielsen@mac.com>
2015-09-15 22:41:24 -1000

    Sprint 2 of website revamp
    Fleshed out members page and playgrounds page.

---
+++ ACMManoa/js/acmmanoa.js
@@ -0,0 +1,17 @@
+(function($) {
+
+$(document)
+    .on( 'hidden.bs.modal', '.modal', function() {
+        $(document.body).removeClass( 'modal-noscrollbar' );
+    })
+    .on( 'show.bs.modal', '.modal', function() {
+        // Bootstrap adds margin-right: 15px to the body to account for a
+        // scrollbar, but this causes a "shift" when the document isn't tall
+        // enough to need a scrollbar; therefore, we disable the margin-right
+        // when it isn't needed.
+        if ( $(window).height() >= $(document).height() ) {
+            $(document.body).addClass( 'modal-noscrollbar' );
+        }
+    });
+
+})(window.jQuery);
